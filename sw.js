a9e371aff340e4f3f2d429e63f4a989b0fdfeca8
ekowsameb-cell <ekowsam.eb@gmail.com>
2026-08-13 22:16:20 +0000

    v2.24.1: neon party background (baked locally, offline-safe, toggle in Boss Settings)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE='fieldbar-v2-1';
-const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./bg-party.jpg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const cp=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return resp;}).catch(()=>caches.match('./index.html'))));});
